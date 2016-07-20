9a9124f86cb0a1dd38e1e25126b997b77a04dfb5
Miguel Xavier <miguelxvr@gmail.com>
2016-07-20 19:29:42 -0300

    Fix script that convert benchmark output to xls

---
+++ scripts/toxls.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOG_DIR=logs
+
+[ ! -d $LOG_DIR ] && mkdir -p $LOG_DIR 
+[ "$(find logs -iname *bench.output)" ] || exit 1
+
+echo "workload;son;h1;h2;wm;wma;pnr" 
+
+ls -A $LOG_DIR/*bench.output | while read line
+do
+	workload=$(echo $line | awk -F"_" '{print $1"_"$2}' | sed 's/^.[^\/]*\///g')
+	egrep -w son[0-9] $line | grep -v PNR | sed 's/.*: //g' | sed 's/ .[^:]*\(:\)/;/g' | sed "s/^/$workload;/g" | sed 's/%//g'
+done
